7013f8eb4bbd9c6f76b9ff6736d00811b319f2c8
Chris Davison <c.jr.davison@gmail.com>
2020-02-06 11:26:17 +0000

    Add ability to filter by filename

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,65 @@ use std::process::Command;
 
 use chrono::prelude::*;
 
-const USAGE: &str = "usage: bookmarks new|list|open|view QUERY...";
+const USAGE: &str = "usage: bookmarks new|list|open|view TAGS... [--title TITLE...]";
+
+struct BMArgs {
+    cmd: String,
+    tag_query: Vec<String>,
+    title_query: Vec<String>,
+}
+
+fn parse_args() -> BMArgs {
+    let cmd: String = args().skip(1).take(1).collect();
+
+    let mut tag_query = Vec::new();
+    let mut title_query = Vec::new();
+    let mut fill_titles_now = false;
+    for arg in args().skip(2) {
+        if arg == "--title" {
+            fill_titles_now = true;
+        } else if fill_titles_now {
+            title_query.push(arg);
+        } else {
+            tag_query.push(arg);
+        }
+    }
+
+    BMArgs{cmd, tag_query, title_query}
+}
 
 fn main() {
-    let cmd: String = args().skip(1).take(1).collect();
-    let query: Vec<String> = args().skip(2).collect();
+    let args = parse_args();
 
-    match cmd.as_ref() {
-        "list"|"" => list(&query),
+    match args.cmd.as_ref() {
+        "list"|"" => list(&args.tag_query, &args.title_query),
         "new" => new(),
-        "open" => open(&query),
-        "view" => view(&query),
-        _ => println!("Unknown cmd '{}'\n\n{}", cmd, USAGE),
+        "open" => open(&args.tag_query, &args.title_query),
+        "view" => view(&args.tag_query, &args.title_query),
+        _ => println!("Unknown cmd '{}'\n\n{}", args.cmd, USAGE),
     };
 }
 
-fn find(query: &[String]) -> Vec<String> {
+fn matches_all_strings(s: String, ss: &[String]) -> bool {
+    for substring in ss {
+        if !s.contains(substring) {
+            return false;
+        }
+    }
+    true
+}
+
+fn find(query: &[String], title_query: &[String]) -> Vec<String> {
     let query: Vec<&str> = query.iter().map(|x| x.as_str()).collect();
     let filt = tagsearch::filter::Filter::new(&query, false);
 
     let bookmarks_dir = format!("{}/Dropbox/bookmarks/", dirs::home_dir().unwrap().to_string_lossy());
     if let Ok(files) = tagsearch::utility::get_files(Some(bookmarks_dir)) {
         match filt.files_matching_tag_query(&files) {
-            Ok(files) => files,
+            Ok(files) => {
+                files.iter().filter(|x| matches_all_strings((*x).to_string(), title_query))
+                .map(|x| x.to_string()).collect()
+            },
             Err(_) => Vec::new(),
         }
     } else {
@@ -34,8 +70,8 @@ fn find(query: &[String]) -> Vec<String> {
     }
 }
 
-fn list(query: &[String]) {
-    let files = find(query);
+fn list(query: &[String], title_query: &[String]) {
+    let files = find(query, title_query);
     if files.is_empty() {
         println!("No bookmarks matching query.");
     } else {
@@ -80,8 +116,8 @@ fn new() {
     std::fs::write(filepath, out).expect("Couldn't write file");
 }
 
-fn view(query: &[String]) {
-    if let Some(entry) = get_choice_from_find(query) {
+fn view(query: &[String], title_query: &[String]) {
+    if let Some(entry) = get_choice_from_find(query, title_query) {
         if let Ok(contents) = std::fs::read_to_string(entry) {
             println!();
             println!("{}", contents);
@@ -89,8 +125,8 @@ fn view(query: &[String]) {
     }
 }
 
-fn open(query: &[String]) {
-    if let Some(entry) = get_choice_from_find(query) {
+fn open(query: &[String], title_query: &[String]) {
+    if let Some(entry) = get_choice_from_find(query, title_query) {
         if let Ok(contents) = std::fs::read_to_string(entry) {
             let url: String = contents.lines().filter(|x| x.starts_with("url")).map(|x| x.split(' ').nth(1).unwrap()).collect();
             if let Err(e) = Command::new("xdg-open").arg(url).status() {
@@ -100,8 +136,8 @@ fn open(query: &[String]) {
     }
 }
 
-fn get_choice_from_find(query: &[String]) -> Option<String> {
-    let files = find(query);
+fn get_choice_from_find(query: &[String], title_query: &[String]) -> Option<String> {
+    let files = find(query, title_query);
     let mut response = String::new();
     if files.is_empty() {
         println!("No files matching query.");
